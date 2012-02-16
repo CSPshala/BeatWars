e3312b566cfdec1e902f397ddedaf86226895ab5
JC Ricks <joe.ricks.csp@gmail.com>
2012-02-15 20:53:43 -0500

    Fleshed out Save/Load.  Now able to load in any song.  Will also get song / song name from XML files now.  Working on keyboard input. - JC

---
+++ BeatMaker/BeatMaker/Beat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeatMaker
+{
+    // Beat Structure, inherits from IComparable to make it comparable to
+    // other beats so I can sort a list by time later.
+    public class Beat : IComparable<Beat>
+    {
+        int nTimeOfBeat;
+        string szDirection;
+        char cKey;
+        string szImage;
+        string szDifficulty;
+        int nWidth;
+        int nHeight;
+
+        public int CompareTo(Beat aBeat)
+        {
+            return nTimeOfBeat.CompareTo(aBeat.nTimeOfBeat);
+        }
+
+    }
+
+}
