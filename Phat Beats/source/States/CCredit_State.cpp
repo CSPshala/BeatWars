0d9e0d316137af7ea5b042eb14f1363b58003482
Jeffery Sventora <JTSventora@LIVE.COM>
2012-03-13 14:57:53 -0400

    Added star wars credits - JTS

--- Phat Beats/source/States/CCredit_State.cpp
+++ Phat Beats/source/States/CCredit_State.cpp
@@ -1,6 +1,9 @@
 #include "CCredit_State.h"
 #include "CMenu_State.h"
+#include <DxErr.h>
+#pragma comment(lib, "dxerr.lib")
 
+#define CUSTOMFVF (D3DFVF_XYZ | D3DFVF_DIFFUSE | D3DFVF_TEX1)
 
 CCredit_State* CCredit_State::GetInstance()
 {
@@ -20,7 +23,8 @@ CCredit_State::CCredit_State()
 
 CCredit_State::~CCredit_State()
 {
-
+	SAFE_RELEASE( quadbuff );
+	SAFE_RELEASE( cubedecl );
 }
 
 
@@ -28,34 +32,35 @@ void CCredit_State::Enter( void )
 {
 	m_nBackgroundID = CSGD_TextureManager::GetInstance()->LoadTexture("resource/graphics/MainMenuBG.jpg");
 	m_nLogo = CSGD_TextureManager::GetInstance()->LoadTexture("resource/graphics/logo_beatWars_1024.png");
-	m_nSoundID = CSGD_FModManager::GetInstance()->LoadSound("resource/Sound/jeditheme.mp3");
+	//m_nSoundID = CSGD_FModManager::GetInstance()->LoadSound("resource/Sound/jeditheme.mp3");
 
-	m_vNames.push_back("programmers");
-	m_vNames.push_back("arron kleker");
-	m_vNames.push_back("jeremy desha");
-	m_vNames.push_back("jeffery sventora");
-	m_vNames.push_back("joe ricks");
-	m_vNames.push_back("ralph concepcion");
-	m_vNames.push_back("aps");
-	m_vNames.push_back("sean hathaway");
-	m_vNames.push_back("shawn paris");
-	m_vNames.push_back("logo model and texture");
-	m_vNames.push_back("jacob browder");
-	m_vNames.push_back("logo modeling");
-	m_vNames.push_back("natasha puckett");
-	m_vNames.push_back("logo  Caricature");
-	m_vNames.push_back("matthew ullrey");
-	m_vNames.push_back("animation and game artist");
-	m_vNames.push_back("chris jahosky");
-	m_vNames.push_back("wapper technology");
-	m_vNames.push_back("dave brown");
-	m_vNames.push_back("particle effects");
-	m_vNames.push_back("jeffery sventora");
-	m_vNames.push_back("animation effects");
-	m_vNames.push_back("ralph concepcion");
-	m_vNames.push_back("beat editor and tech");
-	m_vNames.push_back("joe ricks");
+	HRESULT HR = CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->CreateTexture(512, 1024, 0, D3DUSAGE_RENDERTARGET, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &m_RtText, NULL);
 
+	m_TexCredits = LoadTexture("resource/graphics/Credits.png");
+
+	struct CUSTOMVERTEX t_vert[] =
+	{
+		{5, 0, -15, 0xffffffff, 1, 1,},
+		{-5, 0, -15, 0xffffffff, 0, 1,},
+		{5, 0, 0, 0xffffffff, 1, 0,},
+		{-5, 0, 0, 0xffffffff, 0, 0,},
+	};
+
+     // create a vertex buffer interface called t_buffer
+     CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->CreateVertexBuffer(4*sizeof(CUSTOMVERTEX),
+                                0,
+                                CUSTOMFVF,
+                                D3DPOOL_MANAGED,
+                                &quadbuff,
+                                NULL);
+
+	 // CREATE INDEX BUFFER FOR QUAD
+	 VOID* pVoid;    // a void pointer
+
+     // lock t_buffer and load the vertices into it
+     quadbuff->Lock(0, 0, (void**)&pVoid, 0);
+     memcpy(pVoid, t_vert, sizeof(t_vert));
+     quadbuff->Unlock();
 }
 
 
@@ -73,12 +78,105 @@ void CCredit_State::Update()
 
 void CCredit_State::Render( void )
 {
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetFVF(CUSTOMFVF);
+
+	IDirect3DSurface9* BackBuffer;
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->GetRenderTarget(0, &BackBuffer);
+
+	CSGD_Direct3D::GetInstance()->GetSprite()->End();
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->EndScene();
 	
+	IDirect3DSurface9* RT;
+	m_RtText->GetSurfaceLevel(0, &RT);
+
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetRenderTarget(0, RT);
+	CSGD_Direct3D::GetInstance()->DeviceBegin();
+	CSGD_Direct3D::GetInstance()->SpriteBegin();
+
+	CSGD_Direct3D::GetInstance()->Clear(0, 0, 0);
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+	// Draw Shit HERE
+	RECT Source = {0, 0, 512, 1024};
+	CSGD_Direct3D::GetInstance()->GetSprite()->Draw(m_TexCredits, &Source, NULL, NULL, D3DCOLOR_XRGB(255, 255, 255));
+
+	CSGD_Direct3D::GetInstance()->SpriteEnd();
+	CSGD_Direct3D::GetInstance()->DeviceEnd();
+
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetRenderTarget(0, BackBuffer);
+	CSGD_Direct3D::GetInstance()->DeviceBegin();
+	CSGD_Direct3D::GetInstance()->SpriteBegin();
+	CSGD_Direct3D::GetInstance()->Clear(0, 0, 0);
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+	
+	D3DXMATRIX Identity;
+	D3DXMATRIX Trans;
+	D3DXMATRIX Rot;
+	D3DXMATRIX Combined;
+
+	 // set the view transform
+     D3DXMATRIX matView;    // the view transform matrix
+     D3DXMatrixLookAtLH(&matView,
+     &D3DXVECTOR3 (0.0f, 8.0f, 5.0f),    // the camera position
+     &D3DXVECTOR3 (0.0f, 3.0f, 15.0f),      // the look-at position
+     &D3DXVECTOR3 (0.0f, 1.0f, 0.0f));    // the up direction
+     CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetTransform(D3DTS_VIEW, &matView);    // set the view transform to matView 
+
+    // set the projection transform
+     D3DXMATRIX matProjection;    // the projection transform matrix
+     D3DXMatrixPerspectiveFovLH(&matProjection,
+                                D3DXToRadian(45),    // the horizontal field of view
+                                (FLOAT)800 / (FLOAT)600, // aspect ratio
+                                1.0f,   // the near view-plane
+                                100.0f);    // the far view-plane
+
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetTransform(D3DTS_PROJECTION, &matProjection);
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetStreamSource(0, quadbuff, 0, sizeof(CUSTOMVERTEX));
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetTexture(0, m_RtText);
+
+    D3DXMATRIX matRotateY;
+	D3DXMATRIX trans;
+	static float change = 0.0f; change += CGame::GetInstance()->GetTimer().GetDeltaTime() * 1.0f;
+	D3DXMatrixTranslation(&trans, 0.0f, 0.0f, change);
+	D3DXMatrixRotationX(&matRotateY, D3DXToRadian(0.0f));
+    CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->SetTransform(D3DTS_WORLD, &(matRotateY * trans));    // set the world transform
+	CSGD_Direct3D::GetInstance()->GetDirect3DDevice()->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
 }
 
 
 void CCredit_State::Exit( void )
 {
+	if(m_RtText)
+	{
+		m_RtText->Release();
+		m_RtText = nullptr;
+	}
 
+	if(m_TexCredits)
+	{
+		m_TexCredits->Release();
+		m_TexCredits = nullptr;
+	}
 }
 
+LPDIRECT3DTEXTURE9 CCredit_State::LoadTexture(const char* szFilename, DWORD dwColorkey)
+{
+		//	Make sure the filename is valid.
+	if (!szFilename)	return NULL;
+
+	LPDIRECT3DTEXTURE9 loaded;
+
+	// Load the texture from the given file.
+	HRESULT hr = 0;
+	if (FAILED(hr = D3DXCreateTextureFromFileEx(CSGD_Direct3D::GetInstance()->GetDirect3DDevice(), szFilename, 0, 0, D3DX_DEFAULT, 0,
+		D3DFMT_UNKNOWN, D3DPOOL_MANAGED, D3DX_DEFAULT,
+		D3DX_DEFAULT, dwColorkey, 0, 0, &loaded)))
+	{
+		// Failed.
+		char szBuffer[256] = {0};
+		sprintf(szBuffer, "Failed to Create Texture - %s", szFilename); 
+		MessageBox(0, szBuffer, "TextureManager Error", MB_OK);
+		return NULL;
+	};
+
+	return loaded;
+}
